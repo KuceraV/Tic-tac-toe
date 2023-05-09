1bfc7afd2a5a4d4cb356bffb3c4e6ad31244cc13
Vojtech Kucera <kucerao@outlook.cz>
2023-05-09 21:18:03 +0200

    added final winning condition for the game

--- Tic-tac.toe.py
+++ Tic-tac.toe.py
@@ -37,7 +37,7 @@ marks in a: """,
 welcome_text()
 
 
-def playground(index=" "):
+def playground(index=" ") -> list:
     """"
     Function define the playground  with 9 values(position) of Tic-Tac-Toe as a list
     Space is set as default value in playground, and it is replaced by the symbol
@@ -77,19 +77,34 @@ def write_out_playground(board: list) -> str:
     print(separator_grid)
 
 
-def player_move(board):
+def player_move(board: list):
+    """
+    Function as player to insert number of the move, where to place the symbol.
+    If the input is not digit, function warn player to insert only integer
+    If the input is out of board range, function warn player
+    If the position is already occupied , function warn player
+    """
+
     symbols = ["o", "x"]
     players = {"player_o": "o", "player_x": "x"}
 
     for move in players:
-        index = int(input(f"{move} | Please enter your move number: ")) - 1
+        index = input(f"{move} | Please enter your move number: ")
 
-        if board[index] not in symbols:
-            board[index] = players[move]
-            write_out_playground(board)
+        if index.isdigit():
+            index = int(index)
 
+            if index not in range(1, 10):
+                print("Move number is out of range of the board!")
+
+            elif board[index - 1] not in symbols:
+                board[index - 1] = players[move]
+                write_out_playground(board)
+
+            else:
+                print("Position is already taken!")
         else:
-            print("Position is already taken!")
+            print("Move number has to be integer!")
 
 
 def game(game_is_on):
@@ -98,10 +113,54 @@ def game(game_is_on):
 
     while game_is_on and " " in board:
         player_move(board)
+        win_conditions(board)
+
+    else:
+        print("It is a tie.")
 
 
 game(True)
 
+
+def win_conditions(board):
+    if "x" in board[:3] or "x" in board[4:7] or "x" in board[6:]:
+        print("Player_x wins the game")
+
+    elif "x" in board[0] and "x" in board[3] and "x" in board[6]:
+        print("Player_x wins the game")
+
+    elif "x" in board[1] and "x" in board[4] and "x" in board[7]:
+        print("Player_x wins the game")
+
+    elif "x" in board[2] and "x" in board[5] and "x" in board[8]:
+        print("Player_x wins the game")
+
+    elif "x" in board[0] and "x" in board[4] and "x" in board[8]:
+        print("Player_x wins the game")
+
+    elif "x" in board[2] and "x" in board[4] and "x" in board[6]:
+        print("Player_x wins the game")
+
+    elif "o" in board[:3] or "o" in board[4:7] or "o" in board[6:]:
+        print("Player_o wins the game")
+
+    elif "o" in board[0] and "o" in board[3] and "o" in board[6]:
+        print("Player_o wins the game")
+
+    elif "o" in board[1] and "o" in board[4] and "o" in board[7]:
+        print("Player_o wins the game")
+
+    elif "o" in board[2] and "o" in board[5] and "o" in board[8]:
+        print("Player_o wins the game")
+
+    elif "o" in board[0] and "o" in board[4] and "o" in board[8]:
+        print("Player_o wins the game")
+
+    elif "o" in board[2] and "o" in board[4] and "o" in board[6]:
+        print("Player_o wins the game")
+
+    else:
+        print("lose the game")
 
 # def player_o(board):
 #     symbols = ["x", "o"]
--- test_file.py
+++ test_file.py
@@ -1,18 +1,59 @@
 import os
 
-symbols = ["x", "o"]
-board = [" ", " ", " "]
-game_is_on = True
+# symbols = ["x", "o"]
+# board = [" ", " ", " "]
+# game_is_on = True
+#
+#
+# while game_is_on and " " in board:
+#     player_o = input("Player o | Please enter your move number: ")
+#     if board[int(player_o) - 1] not in symbols:
+#         board[int(player_o) - 1] = "o"
+#         print(board)
+#     else:
+#         print("Position is already taken!")
+# else:
+#     print("Konec hry")
 
+board = ['0', '1', '2', '3', '4', '5', '6', '7', '8']
+win = [1, 2, 3]
 
-while game_is_on and " " in board:
-    os.system("cls")
-    player_o = int(input("Player o | Please enter your move number: ")) - 1
-    if board[player_o] not in symbols:
-        board[player_o ] = "o"
-        print(board)
-    else:
-        print("Position is already taken!")
+if "x" in board[:3] or "x" in board[4:7] or "x" in board[6:]:
+    print("Player_x wins the game")
+
+elif "x" in board[0] and "x" in board[3] and "x" in board[6]:
+    print("Player_x wins the game")
+
+elif "x" in board[1] and "x" in board[4] and "x" in board[7]:
+    print("Player_x wins the game")
+
+elif "x" in board[2] and "x" in board[5] and "x" in board[8]:
+    print("Player_x wins the game")
+
+elif "x" in board[0] and "x" in board[4] and "x" in board[8]:
+    print("Player_x wins the game")
+
+elif "x" in board[2] and "x" in board[4] and "x" in board[6]:
+    print("Player_x wins the game")
+
+elif "o" in board[:3] or "o" in board[4:7] or "o" in board[6:]:
+    print("Player_o wins the game")
+
+elif "o" in board[0] and "o" in board[3] and "o" in board[6]:
+    print("Player_o wins the game")
+
+elif "o" in board[1] and "o" in board[4] and "o" in board[7]:
+    print("Player_o wins the game")
+
+elif "o" in board[2] and "o" in board[5] and "o" in board[8]:
+    print("Player_o wins the game")
+
+elif "o" in board[0] and "o" in board[4] and "o" in board[8]:
+    print("Player_o wins the game")
+
+elif "o" in board[2] and "o" in board[4] and "o" in board[6]:
+    print("Player_o wins the game")
+
 else:
-    print("Konec hry")
+    print("lose the game")
 
