2bb76ad995e89b828a78b9a83b90adb7e5821fb0
Vojtech Kucera <kucerao@outlook.cz>
2023-05-13 22:34:10 +0200

    edited function win_conditions and edited while cycle for the game loop

--- Tic-tac.toe.py
+++ Tic-tac.toe.py
@@ -9,10 +9,13 @@ import os
 
 
 def main():
-    game_is_on = True
+    board = playground()
+    welcome_text()
+    write_out_playground(board)
+    game(board)
 
 
-def welcome_text() -> str:
+def welcome_text():
     """
     function welcome the user a briefly introduce the rules of the game
     """
@@ -34,10 +37,7 @@ marks in a: """,
     )
 
 
-welcome_text()
-
-
-def playground(index=" ") -> list:
+def playground() -> list:
     """"
     Function define the playground  with 9 values(position) of Tic-Tac-Toe as a list
     Space is set as default value in playground, and it is replaced by the symbol
@@ -46,11 +46,11 @@ def playground(index=" ") -> list:
     example: player_x input: 2
     [  ,"x" ,  ,  ,  ,  ,  ,  ,  ]
     """
-    board = [index for i in range(10)]
+    board = [" " for i in range(10)]
     return board
 
 
-def write_out_playground(board: list) -> str:
+def write_out_playground(board: list):
     """"
     function formats the playground to 3 x 3 grid and show current status:
 
@@ -77,6 +77,17 @@ def write_out_playground(board: list) -> str:
     print(separator_grid)
 
 
+def game(board):
+
+    while " " in board and win_conditions(board, "x") and win_conditions(board, "o"):
+        player_move(board)
+        if " " not in board:
+            print("Tie")
+            break
+        if not win_conditions(board, "x") and not win_conditions(board, "o"):
+            break
+
+
 def player_move(board: list):
     """
     Function as player to insert number of the move, where to place the symbol.
@@ -100,6 +111,9 @@ def player_move(board: list):
             elif board[index - 1] not in symbols:
                 board[index - 1] = players[move]
                 write_out_playground(board)
+                if not win_conditions(board, players[move]):
+                    print(f"Congratulations, the player {players[move]} WON!")
+                    break
 
             else:
                 print("Position is already taken!")
@@ -107,82 +121,33 @@ def player_move(board: list):
             print("Move number has to be integer!")
 
 
-def game(game_is_on):
-    board = playground()
-    write_out_playground(board)
+def win_conditions(board: list, symbol: str) -> bool:
+    win = True
+    if symbol == board[0] == board[1] == board[2]:
+        win = False
 
-    while game_is_on and " " in board:
-        player_move(board)
-        win_conditions(board)
+    elif symbol == board[3] == board[4] == board[5]:
+        win = False
 
-    else:
-        print("It is a tie.")
+    elif symbol == board[6] == board[7] == board[8]:
+        win = False
+
+    elif symbol == board[0] == board[3] == board[6]:
+        win = False
+
+    elif symbol == board[1] == board[4] == board[7]:
+        win = False
+
+    elif symbol == board[2] == board[5] == board[8]:
+        win = False
+
+    elif symbol == board[0] == board[4] == board[8]:
+        win = False
+
+    elif symbol == board[2] == board[4] == board[6]:
+        win = False
+
+    return win
 
 
-game(True)
-
-
-def win_conditions(board):
-    if "x" in board[:3] or "x" in board[4:7] or "x" in board[6:]:
-        print("Player_x wins the game")
-
-    elif "x" in board[0] and "x" in board[3] and "x" in board[6]:
-        print("Player_x wins the game")
-
-    elif "x" in board[1] and "x" in board[4] and "x" in board[7]:
-        print("Player_x wins the game")
-
-    elif "x" in board[2] and "x" in board[5] and "x" in board[8]:
-        print("Player_x wins the game")
-
-    elif "x" in board[0] and "x" in board[4] and "x" in board[8]:
-        print("Player_x wins the game")
-
-    elif "x" in board[2] and "x" in board[4] and "x" in board[6]:
-        print("Player_x wins the game")
-
-    elif "o" in board[:3] or "o" in board[4:7] or "o" in board[6:]:
-        print("Player_o wins the game")
-
-    elif "o" in board[0] and "o" in board[3] and "o" in board[6]:
-        print("Player_o wins the game")
-
-    elif "o" in board[1] and "o" in board[4] and "o" in board[7]:
-        print("Player_o wins the game")
-
-    elif "o" in board[2] and "o" in board[5] and "o" in board[8]:
-        print("Player_o wins the game")
-
-    elif "o" in board[0] and "o" in board[4] and "o" in board[8]:
-        print("Player_o wins the game")
-
-    elif "o" in board[2] and "o" in board[4] and "o" in board[6]:
-        print("Player_o wins the game")
-
-    else:
-        print("lose the game")
-
-# def player_o(board):
-#     symbols = ["x", "o"]
-#
-#     player_move = int(input("Player o | Please enter your move number: ")) - 1
-#
-#     if board[player_move] not in symbols:
-#         board[player_move] = "o"
-#     else:  # " " not in board[player_o]:
-#         print("Position is already taken!")
-#     # elif player_o not in range(1, 10):
-#     #     print("Please insert number only in range 1 to 9")
-#
-#
-# def player_x(board):
-#     symbols = ["x", "o"]
-#
-#     player_move = int(input("Player x | Please enter your move number: ")) - 1
-#
-#     if board[player_move] not in symbols:
-#         board[player_move] = "x"
-#     else:  # " " not in board[player_o]:
-#         print("Position is already taken!")
-#     # elif player_o not in range(1, 10):
-#     #     print("Please insert number only in range 1 to 9")
+main()
--- test_file.py
+++ test_file.py
@@ -1,59 +1,35 @@
-import os
+def win_conditions(board: list, symbol):
+    win = True
+    if symbol == board[0] == board[1] == board[2]:
+        win = False
+        print(f"Player {symbol} wins the game")
 
-# symbols = ["x", "o"]
-# board = [" ", " ", " "]
-# game_is_on = True
-#
-#
-# while game_is_on and " " in board:
-#     player_o = input("Player o | Please enter your move number: ")
-#     if board[int(player_o) - 1] not in symbols:
-#         board[int(player_o) - 1] = "o"
-#         print(board)
-#     else:
-#         print("Position is already taken!")
-# else:
-#     print("Konec hry")
+    elif symbol == board[3] == board[4] == board[5]:
+        win = False
+        print(f"Player {symbol} wins the game")
 
-board = ['0', '1', '2', '3', '4', '5', '6', '7', '8']
-win = [1, 2, 3]
+    elif symbol == board[6] == board[7] == board[8]:
+        win = False
+        print(f"Player {symbol} wins the game")
 
-if "x" in board[:3] or "x" in board[4:7] or "x" in board[6:]:
-    print("Player_x wins the game")
+    elif symbol == board[0] == board[3] == board[6]:
+        win = False
+        print(f"Player {symbol} wins the game")
 
-elif "x" in board[0] and "x" in board[3] and "x" in board[6]:
-    print("Player_x wins the game")
+    elif symbol == board[1] == board[4] == board[7]:
+        win = False
+        print(f"Player {symbol} wins the game")
 
-elif "x" in board[1] and "x" in board[4] and "x" in board[7]:
-    print("Player_x wins the game")
+    elif symbol == board[2] == board[5] == board[8]:
+        win = False
+        print(f"Player {symbol} wins the game")
 
-elif "x" in board[2] and "x" in board[5] and "x" in board[8]:
-    print("Player_x wins the game")
+    elif symbol == board[0] == board[4] == board[8]:
+        win = False
+        print(f"Player {symbol} wins the game")
 
-elif "x" in board[0] and "x" in board[4] and "x" in board[8]:
-    print("Player_x wins the game")
+    elif symbol == board[2] == board[4] == board[6]:
+        win = False
+        print(f"Player {symbol} wins the game")
 
-elif "x" in board[2] and "x" in board[4] and "x" in board[6]:
-    print("Player_x wins the game")
-
-elif "o" in board[:3] or "o" in board[4:7] or "o" in board[6:]:
-    print("Player_o wins the game")
-
-elif "o" in board[0] and "o" in board[3] and "o" in board[6]:
-    print("Player_o wins the game")
-
-elif "o" in board[1] and "o" in board[4] and "o" in board[7]:
-    print("Player_o wins the game")
-
-elif "o" in board[2] and "o" in board[5] and "o" in board[8]:
-    print("Player_o wins the game")
-
-elif "o" in board[0] and "o" in board[4] and "o" in board[8]:
-    print("Player_o wins the game")
-
-elif "o" in board[2] and "o" in board[4] and "o" in board[6]:
-    print("Player_o wins the game")
-
-else:
-    print("lose the game")
-
+    return win
